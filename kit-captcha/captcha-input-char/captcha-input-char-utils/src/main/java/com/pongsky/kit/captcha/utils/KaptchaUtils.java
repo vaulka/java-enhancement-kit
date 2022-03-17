84d9c676db57991eb129a4303b6ee54516d30300
彭森豪 <kelry@vip.qq.com>
2022-03-17 13:59:52 +0800

    perf(java-enhancement-kit): 优化 输入型 验证码 生成的图像

--- kit-captcha/captcha-input-char/captcha-input-char-utils/src/main/java/com/pongsky/kit/captcha/utils/KaptchaUtils.java
+++ kit-captcha/captcha-input-char/captcha-input-char-utils/src/main/java/com/pongsky/kit/captcha/utils/KaptchaUtils.java
@@ -1,14 +1,12 @@
 package com.pongsky.kit.captcha.utils;
 
-import com.google.code.kaptcha.impl.DefaultKaptcha;
-import com.google.code.kaptcha.util.Config;
-
 import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.Base64;
-import java.util.Properties;
+import java.util.Random;
 
 /**
  * 输入型字符 验证码工具类
@@ -17,22 +15,90 @@ import java.util.Properties;
  */
 public class KaptchaUtils {
 
-    DefaultKaptcha defaultKaptcha;
+    /**
+     * 最大颜色数
+     */
+    private static final int MAX_COLOR_NUM = 255;
 
-    public KaptchaUtils(Properties properties) {
-        defaultKaptcha = new DefaultKaptcha();
-        Config config = new Config(properties);
-        defaultKaptcha.setConfig(config);
+    /**
+     * 取得给定范围随机颜色
+     *
+     * @param fc fc
+     * @param bc bc
+     * @return 颜色
+     */
+    private Color getRandColor(int fc, int bc) {
+        final Random random = new Random();
+        if (fc > MAX_COLOR_NUM) {
+            fc = MAX_COLOR_NUM;
+        }
+        if (bc > MAX_COLOR_NUM) {
+            bc = MAX_COLOR_NUM;
+        }
+        final int r = fc + random.nextInt(bc - fc);
+        final int g = fc + random.nextInt(bc - fc);
+        final int b = fc + random.nextInt(bc - fc);
+        return new Color(r, g, b);
     }
 
     /**
-     * 创建验证码 图像类型
+     * 定义图形大小
+     */
+    private static final int IMAGE_WIDTH = 105;
+    /**
+     * 定义图形大小
+     */
+    private static final int IMAGE_HEIGHT = 35;
+
+    /**
+     * 定义干扰线数量
+     */
+    private static final int DRAW_COUNT = 200;
+
+    /**
+     * 干扰线的长度 = 1.414 * LINE_WIDTH
+     */
+    private static final int LINE_WIDTH = 2;
+
+    /**
+     * 创建图像验证码
      *
      * @param code 验证码
-     * @return 创建验证码 图像类型
+     * @return 创建图像验证码
      */
-    public BufferedImage createByImage(String code) {
-        return defaultKaptcha.createImage(code);
+    private BufferedImage createImage(String code) {
+        // 在内存中创建图像
+        final BufferedImage image = new BufferedImage(IMAGE_WIDTH, IMAGE_HEIGHT, BufferedImage.TYPE_INT_RGB);
+        // 获取图形上下文
+        final Graphics2D graphics = (Graphics2D) image.getGraphics();
+        // 设定背景颜色
+        graphics.setColor(Color.WHITE);
+        graphics.fillRect(0, 0, IMAGE_WIDTH, IMAGE_HEIGHT);
+        graphics.drawRect(0, 0, IMAGE_WIDTH - 1, IMAGE_HEIGHT - 1);
+        final Random random = new Random();
+        // 随机产生干扰线，使图像中的认证码不易被其它程序探测到
+        for (int i = 0; i < DRAW_COUNT; i++) {
+            graphics.setColor(getRandColor(150, 200));
+            // 保证画在边框之内
+            final int x = random.nextInt(IMAGE_WIDTH - LINE_WIDTH - 1) + 1;
+            final int y = random.nextInt(IMAGE_HEIGHT - LINE_WIDTH - 1) + 1;
+            final int xl = random.nextInt(LINE_WIDTH);
+            final int yl = random.nextInt(LINE_WIDTH);
+            graphics.drawLine(x, y, x + xl, y + yl);
+        }
+        // 取随机产生的认证码
+        for (int i = 0; i < code.length(); i++) {
+            // 将认证码显示到图像中,调用函数出来的颜色相同，可能是因为种子太接近，所以只能直接生成
+            // 设置字体颜色
+            graphics.setColor(Color.BLACK);
+            // 设置字体样式
+            graphics.setFont(new Font("Times New Roman", Font.BOLD, 24));
+            // 设置字符，字符间距，上边距
+            graphics.drawString(String.valueOf(code.charAt(i)), (23 * i) + 8, 26);
+        }
+        // 图像生效
+        graphics.dispose();
+        return image;
     }
 
     /**
@@ -41,13 +107,13 @@ public class KaptchaUtils {
     private static final String FORMAT_NAME = "jpg";
 
     /**
-     * 创建验证码 OutputStream 流
+     * 创建图像验证码 Stream 流
      *
      * @param code 验证码
-     * @return 创建验证码 OutputStream 流
+     * @return 创建验证码 Stream 流
      */
-    public ByteArrayOutputStream createByOutputStream(String code) throws IOException {
-        BufferedImage image = defaultKaptcha.createImage(code);
+    public ByteArrayOutputStream createImageByStream(String code) throws IOException {
+        BufferedImage image = this.createImage(code);
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         try (outputStream) {
             ImageIO.write(image, FORMAT_NAME, outputStream);
@@ -56,17 +122,13 @@ public class KaptchaUtils {
     }
 
     /**
-     * 创建验证码 Base64 信息
+     * 创建图像验证码 Base64 信息
      *
      * @param code 验证码
      * @return 创建验证码 Base64 信息
      */
-    public String createByBase64(String code) throws IOException {
-        BufferedImage image = defaultKaptcha.createImage(code);
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        try (outputStream) {
-            ImageIO.write(image, FORMAT_NAME, outputStream);
-        }
+    public String createImageByBase64(String code) throws IOException {
+        ByteArrayOutputStream outputStream = this.createImageByStream(code);
         return Base64.getEncoder().encodeToString(outputStream.toByteArray());
     }
 
--- kit-captcha/captcha-input-char/spring-boot-starter-captcha-input-char/src/main/java/com/pongsky/kit/captcha/config/KaptchaConfiguration.java
+++ kit-captcha/captcha-input-char/spring-boot-starter-captcha-input-char/src/main/java/com/pongsky/kit/captcha/config/KaptchaConfiguration.java
@@ -1,10 +1,7 @@
 package com.pongsky.kit.captcha.config;
 
 import com.pongsky.kit.captcha.utils.KaptchaUtils;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
-
-import java.util.Properties;
 
 
 /**
@@ -16,114 +13,12 @@ public class KaptchaConfiguration {
 
     /**
      * 创建验证码工具类
-     * <p>
-     * 如需设置验证码参数，可自行创建该 bean
      *
      * @return 验证码工具类
      */
-    @ConditionalOnMissingBean(KaptchaUtils.class)
     @Bean
     public KaptchaUtils kaptchaUtils() {
-        Properties properties = new Properties();
-
-        // 图片边框
-        // 合法值：yes / no
-        // 默认值：yes
-        properties.setProperty("kaptcha.border", "yes");
-
-        // 边框颜色
-        // 合法值：r,g,b / white,black,blue.
-        // 默认值：black
-        properties.setProperty("kaptcha.border.color", "black");
-
-        // 边框厚度
-        // 合法值：> 0
-        // 默认值：1
-        properties.setProperty("kaptcha.border.thickness", "1");
-
-        // 图片宽度
-        // 默认值：200
-        properties.setProperty("kaptcha.image.width", "200");
-
-        // 图片高度
-        // 默认值：50
-        properties.setProperty("kaptcha.image.height", "50");
-
-        // 图片实现类
-        // 默认值：com.google.code.kaptcha.impl.DefaultKaptcha
-        properties.setProperty("kaptcha.producer.impl", "com.google.code.kaptcha.impl.DefaultKaptcha");
-
-        // 文本实现类
-        // 默认值：com.google.code.kaptcha.text.impl.DefaultTextCreator
-        properties.setProperty("kaptcha.textproducer.impl", "com.google.code.kaptcha.text.impl.DefaultTextCreator");
-
-        // 文本集合，验证码值从此集合中获取
-        // 默认值：abcde2345678gfynmnpwx
-        properties.setProperty("kaptcha.textproducer.char.string", "abcde2345678gfynmnpwx");
-
-        // 验证码长度
-        // 默认值：5
-        properties.setProperty("kaptcha.textproducer.char.length", "5");
-
-        // 文字间隔
-        // 默认值：2
-        properties.setProperty("kaptcha.textproducer.char.space", "2");
-
-        // 字体，多个,间隔
-        // 默认值：Arial,Courier
-        properties.setProperty("kaptcha.textproducer.font.names", "Arial,Courier");
-
-        // 字体大小，单位 px
-        // 默认值：40
-        properties.setProperty("kaptcha.textproducer.font.size", "40");
-
-        // 字体颜色
-        // 合法值：r,g,b / white,black,blue
-        // 默认值：black
-        properties.setProperty("kaptcha.textproducer.font.color", "black");
-
-        // 干扰实现类
-        // 默认值：com.google.code.kaptcha.impl.DefaultNoise
-        properties.setProperty("kaptcha.noise.impl", "com.google.code.kaptcha.impl.DefaultNoise");
-
-        // 干扰颜色
-        // 合法值：r,g,b / white,black,blue
-        // 默认值：black
-        properties.setProperty("kaptcha.noise.color", "black");
-
-        // 图片样式
-        // 合法值：
-        // 水纹：com.google.code.kaptcha.impl.WaterRipple
-        // 鱼眼：com.google.code.kaptcha.impl.FishEyeGimpy
-        // 阴影：com.google.code.kaptcha.impl.ShadowGimpy
-        // 默认值：com.google.code.kaptcha.impl.WaterRipple
-        properties.setProperty("kaptcha.obscurificator.impl", "com.google.code.kaptcha.impl.WaterRipple");
-
-        // 背景实现类
-        // 默认值：com.google.code.kaptcha.impl.DefaultBackground
-        properties.setProperty("kaptcha.background.impl", "com.google.code.kaptcha.impl.DefaultBackground");
-
-        // 背景颜色渐变，开始颜色
-        // 默认值：lightGray
-        properties.setProperty("kaptcha.background.clear.from", "lightGray");
-
-        // 背景颜色渐变，结束颜色
-        // 默认值：white
-        properties.setProperty("kaptcha.background.clear.to", "white");
-
-        // 文字渲染器
-        // 默认值：com.google.code.kaptcha.text.impl.DefaultWordRenderer
-        properties.setProperty("kaptcha.word.impl", "com.google.code.kaptcha.text.impl.DefaultWordRenderer");
-
-        // session key
-        // 默认值：KAPTCHA_SESSION_KEY
-        properties.setProperty("kaptcha.session.key", "KAPTCHA_SESSION_KEY");
-
-        // session date
-        // 默认值：KAPTCHA_SESSION_DATE
-        properties.setProperty("kaptcha.session.date", "KAPTCHA_SESSION_DATE");
-
-        return new KaptchaUtils(properties);
+        return new KaptchaUtils();
     }
 
 }
