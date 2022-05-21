737cef14b3fe1b02b22a92209b2b502852fe53c8
彭森豪 <kelry@vip.qq.com>
2022-05-21 19:39:41 +0800

    feat(spring-boot-starter-knife4j): 添加 Knife4j 自动装配

---
+++ kit-doc/spring-boot-starter-knife4j/src/main/java/com/pongsky/kit/knife4j/autoconfigure/Knife4jAutoConfiguration.java
@@ -0,0 +1,16 @@
+package com.pongsky.kit.knife4j.autoconfigure;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Knife4j 自动装配
+ *
+ * @author pengsenhao
+ **/
+@RequiredArgsConstructor
+@Configuration(proxyBeanMethods = false)
+public class Knife4jAutoConfiguration {
+
+
+}
