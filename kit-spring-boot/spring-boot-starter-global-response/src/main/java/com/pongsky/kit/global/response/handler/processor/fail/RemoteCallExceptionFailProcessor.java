cd9cb01fc9565920ed3979b6434644467673f1aa
彭森豪 <kelry@vip.qq.com>
2022-06-01 11:42:30 +0800

    fix(spring-boot-starter-global-response): 远程调用异常处理器

--- kit-spring-boot/spring-boot-starter-global-response/src/main/java/com/pongsky/kit/global/response/handler/processor/fail/RemoteCallExceptionFailProcessor.java
+++ kit-spring-boot/spring-boot-starter-global-response/src/main/java/com/pongsky/kit/global/response/handler/processor/fail/RemoteCallExceptionFailProcessor.java
@@ -2,12 +2,15 @@ package com.pongsky.kit.global.response.handler.processor.fail;
 
 import com.pongsky.kit.common.exception.RemoteCallException;
 import com.pongsky.kit.common.global.response.processor.fail.BaseFailProcessor;
+import feign.FeignException;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 
 /**
  * 远程调用异常处理器
  *
  * @author pengsenhao
  */
+@ConditionalOnClass(FeignException.class)
 public class RemoteCallExceptionFailProcessor implements BaseFailProcessor<RemoteCallException> {
 
     @Override
